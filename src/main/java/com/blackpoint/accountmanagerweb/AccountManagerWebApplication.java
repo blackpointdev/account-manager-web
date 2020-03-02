cf55fc5a2e358bbb9358a85e11b9e36023914d27
blackpoint <marcinw1998@gmail.com>
2020-03-02 23:40:13 +0100

    Initial commit.

---
+++ src/main/java/com/blackpoint/accountmanagerweb/AccountManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.blackpoint.accountmanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackpoint/accountmanagerweb/AccountManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackpoint.accountmanagerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
