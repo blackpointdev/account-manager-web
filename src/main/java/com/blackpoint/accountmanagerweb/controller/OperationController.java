84bae32a33d13d2b49a6034142e5597cf512bdc8
blackpoint <marcinw1998@gmail.com>
2020-03-18 20:14:12 +0100

    Added OperationController, changed PaddingandSorting repo to JpaRepo for UserRepo.

--- src/main/java/com/blackpoint/accountmanagerweb/controller/OperationController.java
+++ src/main/java/com/blackpoint/accountmanagerweb/controller/OperationController.java
@@ -1,8 +1,45 @@
 package com.blackpoint.accountmanagerweb.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.blackpoint.accountmanagerweb.exception.ResourceNotFoundException;
+import com.blackpoint.accountmanagerweb.model.Operation;
+import com.blackpoint.accountmanagerweb.model.User;
+import com.blackpoint.accountmanagerweb.repository.OperationRepository;
+
+import com.blackpoint.accountmanagerweb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class OperationController {
-    
+
+    @Autowired
+    private OperationRepository operationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/operations")
+    public Page<Operation> getAllOperations(Pageable pageable) {
+        return operationRepository.findAll(pageable);
+    }
+
+    @GetMapping("/users/{username}/operations")
+    public Page<Operation> getOperationsByUsername(@PathVariable String username, Pageable pageable) {
+        return operationRepository.findByUserUsername(username, pageable);
+    }
+
+    @PostMapping("/users/{username}/operations")
+    public Operation addOperation(@Valid @RequestBody Operation operationRequest,
+                                  @PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) throw new ResourceNotFoundException("User not found with given username: " + username);
+
+        operationRequest.setUser(user);
+        return operationRepository.save(operationRequest);
+    }
 }
