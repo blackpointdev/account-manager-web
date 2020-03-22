6a4a894a31676c1470be229dd396bdf280887aa2
blackpointdev <marcinw1998@gmail.co>
2020-03-22 16:26:36 +0100

    Added PUT and DELETE methods to Operation and User controllers.

--- src/main/java/com/blackpoint/accountmanagerweb/controller/OperationController.java
+++ src/main/java/com/blackpoint/accountmanagerweb/controller/OperationController.java
@@ -9,10 +9,11 @@ import com.blackpoint.accountmanagerweb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class OperationController {
@@ -28,6 +29,12 @@ public class OperationController {
         return operationRepository.findAll(pageable);
     }
 
+    @GetMapping("/operations/{operationId}")
+    public Operation getOperationById(@PathVariable Long operationId) {
+        return operationRepository.findById(operationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Operation not found with given id: " + operationId));
+    }
+
     @GetMapping("/users/{username}/operations")
     public Page<Operation> getOperationsByUsername(@PathVariable String username, Pageable pageable) {
         return operationRepository.findByUserUsername(username, pageable);
@@ -41,5 +48,30 @@ public class OperationController {
 
         operationRequest.setUser(user);
         return operationRepository.save(operationRequest);
+    }
+
+    @PutMapping("/operations/{operationId}")
+    public Operation updateOperations(@PathVariable Long operationId,
+                                      @Valid @RequestBody Operation operationRequest) {
+        Optional<Operation> optionalOperation = operationRepository.findById(operationId);
+
+        return optionalOperation.map(operation -> {
+           operation.setName(operationRequest.getName());
+           operation.setBalance(operationRequest.getBalance());
+           if (operationRequest.getUser() != null) {
+               operation.setUser(operationRequest.getUser());
+           }
+           return operationRepository.save(operation);
+        }).orElseThrow(() -> new ResourceNotFoundException("Operation not found with id: " + operationId));
+    }
+
+    @DeleteMapping("/operations/{operationId}")
+    public ResponseEntity<?> deleteOperation(@PathVariable Long operationId) {
+        Optional<Operation> optionalOperation = operationRepository.findById(operationId);
+
+        return optionalOperation.map(operation -> {
+            operationRepository.delete(operation);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("Operation not found with id: " + operationId));
     }
 }
