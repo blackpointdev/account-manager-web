a6190761749d67d66c5a0253ddd6ee1e6d52bcc7
blackpointdev <marcinw1998@gmail.co>
2020-03-23 17:52:08 +0100

    Minor fixes to routing.

--- src/main/java/com/blackpoint/accountmanagerweb/controller/UserController.java
+++ src/main/java/com/blackpoint/accountmanagerweb/controller/UserController.java
@@ -13,6 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired //TODO Maybe change to some kind of constructor injection?
